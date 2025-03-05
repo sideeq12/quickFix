9672d1040bfd9e3f463ee1d3ec33e698587ca589
sideeq12 <abdwaheed2018@gmail.com>
2025-03-05 04:54:18 -0600

    footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
       <div className="container border-b py-10 mx-auto w-5/6 
       gap-8 grid grid-cols-3 lg:grid-cols-4 justify-between ">
         <div className=' text-left col-span-2'>
-         <img src="/logo.png" className='h-12' alt="" />
+         <img src="/footerLogo.png" className='h-12' alt="" />
           <p className="text-sm mt-2">    BloodHero is dedicated to connecting blood donors with recipients in need. Our platform ensures that those in urgent need of blood can find donors quickly and efficiently. Join us in saving lives and making a difference in your community.
           </p>
         </div>
