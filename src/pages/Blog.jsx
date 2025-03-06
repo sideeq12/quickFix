f4156627234a9d1b759e2448bc3574803330c346
sideeq12 <abdwaheed2018@gmail.com>
2025-03-05 19:50:59 -0600

    nav

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -25,13 +25,14 @@ const Blog = () => {
         <div className='flex flex-wrap -mt-4 w-5/6 mx-auto gap-3 justify-center'>
        { filter.map((filter, index)=><div className='hover:bg-red-500 cursor-pointer  bg-[#FFBFBF] text-red-500  hover:text-white h-fit rounded-md px-5 py-2 text-sm'>{filter}</div>)
        } </div>
-             <div className='grid grid-cols-2 bg-[#FDE2E2] p-2
-             my-10 w-full lg:w-5/6 mx-auto text-left align-middle  rounded-lg justify-center gap-2 lg:gap-10'>
+             <div className='grid grid-cols-2 bg-[#FDE2E2] p-2 my-10
+              w-full lg:w-5/6 mx-auto text-left align-middle  rounded-lg justify-center gap-2 lg:gap-10'>
         <div className=' leading-[30px] col-span-1'>
       <h3 className='text-sm lg:text-xl font-semibold lg:my-4 px-2 lg:px-10'>The Lifeline of Humanity: 
         Understanding the Power of Blood Donation.</h3>
         <p className='px-2 text-xs lg:text-lg lg:px-10'>
-        Blood is the essence of life. Every day, thousands of people around the world rely on blood  ...
+        Blood is the essence of life. Every day, thousands of people around the world rely on blood and the next otlining 
+        methodology  ...
          </p>
          <div className='flex gap-3 pl-2 lg:pl-10 align-middle w-full justify-between'>
           <div>by Aishat</div>
